4ccb97196dc71b620995f81f7f7c3ef24e30b92c
Jay Evans <lyndenjayevans@gmail.com>
2019-08-26 23:30:41 -0400

    workinggit add .!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var http = require('http').createServer(app);
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://joshua:<password>@cluster0-ubdqx.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://joshua:upworkbattleio@cluster0-ubdqx.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 
@@ -14,18 +14,19 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('*/new', function(req, res){
+app.get('/new', function(req, res){
   client.connect(err => {
-  const collection = client.db("test").collection("wabbitseason");
-  var entry1 = {im : 'hunting', some: 'wabbits'};
-  var entry2 = {nah : 'whats', up: 'doc'};
-  // perform actions on the collection object
-  collection.insert(entry1);
-  collection.insert(entry2);
-  client.close();
+  
   if (err){
     res.send(err);		
   } else {
+const collection = client.db("test").collection("wabbitseason");
+  var entry1 = {im : 'not hunting', some: 'wabbits'};
+  var entry2 = {nah : 'whats not', up: 'doc'};
+  // perform actions on the collection object
+  collection.insertOne(entry1);
+  collection.insertOne(entry2);
+  client.close();
     res.send("IT WORKED");
   }
 });
